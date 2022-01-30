14574df4b5d142de91db71f63a1e0c35a8a3ba1a
PatrickNausha <patrick.nausha@gmail.com>
2022-01-30 12:27:59 -0800

    Add count text.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 0;
+    public TextMeshProUGUI CountText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -17,6 +20,11 @@ public class PlayerController : MonoBehaviour
         var momentVector = movementValue.Get<Vector2>();
         movementX = momentVector.x;
         movementY = momentVector.y;
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
     }
 
     void FixedUpdate()
@@ -27,10 +35,15 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pickup"))
+        {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count = 0;
 }
